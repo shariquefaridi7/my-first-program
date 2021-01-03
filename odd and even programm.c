4999c936ac60f0f7514215b112f49d1c232ea093
shariquefaridi <shariquefaridi786@gmail.com>
2021-01-04 03:25:51 +0530

    comitt

---
+++ odd and even programm.c
@@ -0,0 +1,22 @@
+/*
+code for odd and even
+*/
+
+#include<stdio.h>
+
+void main()
+{
+
+
+int a;
+printf("enter the value of a ");
+  scanf("%d",&a);
+  if(a%2==0)
+  {
+  printf("it is even number");
+}
+else   
+{
+	printf("it is odd number");
+}
+}
