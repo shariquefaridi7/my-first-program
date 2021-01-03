c6d6b50e1cb49f8e3d7ab2c0a5d8634616e5b638
shariquefaridi <shariquefaridi786@gmail.com>
2021-01-04 02:44:24 +0530

    commit

---
+++ pointer in c programm.c
@@ -0,0 +1,19 @@
+/*
+pointer in c programm
+*/
+#include<stdio.h>
+
+void main()
+{
+int a=55,*p,*q,b=57;
+q=&b;
+p=&a;
+printf("the value of a %d,%u\n",a,&a);
+printf("the address of a %d\n",&a);
+printf("%d\n",*p);	
+printf("%d\n",&*p);
+printf("%d\n",&a+1);	
+printf("%u\n",);
+printf("%u",&b);
+ } 
+
