e9147bb0cbb66c3c2faf44f0690000fd3c2439c9
shariquefaridi <shariquefaridi786@gmail.com>
2021-01-10 15:30:05 +0530

    master

---
+++ square of any number.c
@@ -0,0 +1,15 @@
+/*
+square of any number in c program
+*/
+#include<stdio.h>
+#include<math.h>
+
+void main()
+{
+	int a, square;
+      printf("enter the value of a = ");
+        scanf("%d",&a);
+     square=sqrt(a);
+	 printf("square of a = %d",square);
+             
+}
