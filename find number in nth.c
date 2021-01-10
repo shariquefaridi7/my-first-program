0046a0ff838fbb9610d7529556a17f32fa411a59
shariquefaridi <shariquefaridi786@gmail.com>
2021-01-10 14:58:04 +0530

    commit

---
+++ find number in nth.c
@@ -0,0 +1,18 @@
+/*
+find the numbers to the nth number in c language
+*/
+
+#include<stdio.h>
+
+void main()
+{
+	int a,i;
+	printf("enter the value of a = ");
+	 scanf("%d",&a);
+	 
+	 for(i=1;i<=a;i++)
+	 {
+	 	printf("%d\t",i);
+	 }
+	 
+}
