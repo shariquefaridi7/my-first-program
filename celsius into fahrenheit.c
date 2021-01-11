c8ca56d1fe19eea070f216566f437d911462cb4e
shariquefaridi <shariquefaridi786@gmail.com>
2021-01-11 21:41:40 +0530

    commit

---
+++ celsius into fahrenheit.c
@@ -0,0 +1,17 @@
+/*
+convert  celsius into fahrenheit  in c language
+*/
+#include<stdio.h>
+
+void main()
+{     
+
+	int celsius,fahrenheit;
+    	printf("enter the value of celsius = ");
+        	scanf("%d",&celsius);
+	
+    	fahrenheit=(celsius*9/5)+32;
+	
+    	printf("enter the value of fahrenheit = %d",fahrenheit);
+	
+}
