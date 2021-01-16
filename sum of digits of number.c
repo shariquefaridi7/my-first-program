3e48667bd54b94db6fa938c394b503188ae4ceb8
shariquefaridi <shariquefaridi786@gmail.com>
2021-01-16 23:17:34 +0530

    commit

---
+++ sum of digits of number.c
@@ -0,0 +1,19 @@
+ /*
+ the sum of digit of a number in c language
+ */
+ #include<stdio.h>
+ void main()
+ {
+ 	int a,r,s=0,c;
+ 	printf("Enter the value of a = ");
+ 	scanf("%d",&a);
+ 	
+ 	while(a>0)
+ 	{ 
+ 	  r=a%10;ew
+	   s=s+c;
+	   a=a/10;                                                                                                                                                                      	
+	 }
+	 printf("sum of digit = %d",s);
+	 
+ } 
